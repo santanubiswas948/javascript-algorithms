3e73a4126aed5a5b8f55b4e8b9c8cb155afbe0d9
Santanu Biswas <35422871+santanubiswas948@users.noreply.github.com>
2021-12-03 20:32:15 +0530

    all possible subsets of n items: complexity O(2^n)

--- src/backtracking/allPossibleSubsetsWithRecursive.js
+++ src/backtracking/allPossibleSubsetsWithRecursive.js
@@ -3,8 +3,8 @@ This is a programme for get all possible subsets from n items.
 This problem solved using recursive way.
 Complexity: O(2^n)
 example:
-items: n = [1,2,3]
-possible subsets: ["", "3", "2", "23", "1", "13", "12", "123"]
+  items: n = [1,2,3]
+  possible subsets: ["", "3", "2", "23", "1", "13", "12", "123"]
 */
 
 function backTracking(arr, curInd, currPath, allSubsets){
