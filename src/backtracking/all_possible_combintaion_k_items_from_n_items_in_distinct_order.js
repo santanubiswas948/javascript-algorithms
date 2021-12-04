b39e7fd4082c2d1c44a89f11826527afa0073ef0
Santanu Biswas <35422871+santanubiswas948@users.noreply.github.com>
2021-12-04 08:57:55 +0530

    possible combinations of k items in distinct order from n items with Complexity O(nPk)

---
+++ src/backtracking/all_possible_combintaion_k_items_from_n_items_in_distinct_order.js
@@ -0,0 +1,23 @@
+/*
+This is programming for get all possible combinations of k items in distinct order from n items
+This is same as total possible combinations =  nPk = n!/(n - k)!
+Complexity: O(nPk)
+example: n = [1,2,3]
+possible combinations in distinct order of k = 2 items from this n items are: ["12", "13", "21", "23", "31", "32"]
+
+*/
+function backTracking(arr, currPath, allSubsets, k){
+  var i = 0;
+  while(i<arr.length && currPath.length < k){
+    // check unvisited nodes
+    if (!currPath.includes(arr[i])) {
+      backTracking(arr,currPath + arr[i], allSubsets, k); 
+    }
+    i++;
+  }
+  if(currPath.length === k){
+    allSubsets.push(currPath);
+  }
+  return allSubsets;
+}
+// console.log(backTracking([1,2,3],'', [], 2));
